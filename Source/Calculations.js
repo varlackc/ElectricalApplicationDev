b3653dc4b748207fac83a4887b48fac3225ab05b
Maxwell Varlack <varlackc@mail.gvsu.edu>
2020-09-30 22:15:12 -0700

    Update Calculations.js

--- Source/Calculations.js
+++ Source/Calculations.js
@@ -578,7 +578,6 @@
                 document.getElementById("current6").disabled = false;
                 document.getElementById("efficiency6").disabled = false;
                 document.getElementById("horsePower6").disabled = false;
-
                 //change the color of the answer
                 document.getElementById("voltage6").style.backgroundColor = "#9AD58E";
                 document.getElementById("current6").style.backgroundColor = "#9AD58E";
