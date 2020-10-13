be79d5e43922a7bb279e94d297118b496db89af1
Maxwell Varlack <varlackc@mail.gvsu.edu>
2020-10-12 22:17:20 -0700

    Update JsonValues.js

--- Source/JsonValues.js
+++ Source/JsonValues.js
@@ -1,7 +1,7 @@
 // This section will give the Json Values to the rest of the application
 
-var ResistorValues = "";
+var ResistorValues = "{}";
 
-var CapacitorValues = "";
+var CapacitorValues = "{}";
 
-var InductorValues = "";
+var InductorValues = "{}";
