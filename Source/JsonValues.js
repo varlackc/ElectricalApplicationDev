253bcfa0654dd1b8846b0744e4f06b205f6108ce
Maxwell Varlack <varlackc@mail.gvsu.edu>
2020-10-13 22:43:29 -0700

    Update JsonValues.js

--- Source/JsonValues.js
+++ Source/JsonValues.js
@@ -1,9 +1,9 @@
 // This section will give the Json Values to the rest of the application
 
 var ResistorValues = {
-                        "Value": "10", "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
-                        "Value": "15", "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
-                        "Value": "22", "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
+                        "Value": "10", "ColorDigitOne": 'brown', "ColorDigitTwo": 'black', "Multiplier": 'black', 
+                        "Value": "15", "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , 
+                        "Value": "22", "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , 
                         "Value": "33", "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
                         "Value": "47", "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
                         "Value": "68", "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
