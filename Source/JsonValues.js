01134466f46d407ad0ce1712f91a711c8481974b
Maxwell Varlack <varlackc@mail.gvsu.edu>
2020-10-13 22:14:03 -0700

    Update JsonValues.js

--- Source/JsonValues.js
+++ Source/JsonValues.js
@@ -1,6 +1,29 @@
 // This section will give the Json Values to the rest of the application
 
 var ResistorValues = {
+                        "Value": "10", "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
+                        "Value": "15", "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
+                        "Value": "22", "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
+                        "Value": "33", "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
+                        "Value": "47", "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
+                        "Value": "68", "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
+                        "Value": , "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
+                        "Value": , "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
+                        "Value": , "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
+                        "Value": , "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
+                        "Value": , "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
+                        "Value": , "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
+                        "Value": , "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
+                        "Value": , "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
+                        "Value": , "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
+                        "Value": , "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
+                        "Value": , "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
+                        "Value": , "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
+                        "Value": , "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
+                        "Value": , "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
+                        "Value": , "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
+                        "Value": , "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
+                        "Value": , "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
                         "Value": , "ColorDigitOne": , "ColorDigitTwo": , "Multiplier": , "Tolerance": ,
                      };
 
