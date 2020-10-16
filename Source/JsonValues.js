dceea76ffc708eb2b11a84e67db97b291b57b679
Maxwell Varlack <varlackc@mail.gvsu.edu>
2020-10-15 23:22:43 -0700

    Update JsonValues.js

--- Source/JsonValues.js
+++ Source/JsonValues.js
@@ -66,6 +66,7 @@ var CapacitorValues = {
                         "Value": ,
                         "Value": ,
                         "Value": ,
+                        "Value": ,
                       };
 
 var InductorValues = {
