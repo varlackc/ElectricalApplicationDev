4419fdb38741f93dd33ae8de3660d73f1a3d58cd
Maxwell Varlack <varlackc@mail.gvsu.edu>
2020-10-16 22:21:29 -0700

    Create Data.js

---
+++ Source/Data.js
@@ -0,0 +1,24 @@
+// This section will give the Json Values to the rest of the application
+
+var ResistorValues = "This is external Data";
+
+console.log(ResistorValues);
+/*
+var CapacitorValues = {
+                        "Value": ,
+                        "Value": ,
+                        "Value": ,
+                        "Value": ,
+                        "Value": ,
+                        "Value": ,
+                        "Value": ,
+                        "Value": ,
+                        "Value": ,
+                        "Value": ,
+                        "Value": ,
+                      };
+
+var InductorValues = {
+                        "Value": , "ColorDigitOne": , "ColorDigitTwo:" , "Multiplier": , "Tolerance": ,
+                     };
+                     */
