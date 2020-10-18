ef6a2fae33645819fffe1cdcee7c9b66d067aed7
Maxwell Varlack <varlackc@mail.gvsu.edu>
2020-10-17 23:24:23 -0700

    Update Calculations.js

--- Source/Calculations.js
+++ Source/Calculations.js
@@ -1,6 +1,5 @@
 // Import external JS file using JQuery
 //$.getScript("~/JsonValues.js", function(){});\
-//console.log("About to fetch data")
 
     // Declare variables
     var calculatedGlobal = false; // reset value
